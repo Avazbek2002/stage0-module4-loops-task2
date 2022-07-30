712fa646f675fc8d539d63a3564adb4f5300122d
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-07-30 17:55:34 +0500

    hello

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
-        for (int i = 0; i <= printTillInclusive; i += 2) {
+        for (int i = 2; i <= printTillInclusive; i += 2) {
             System.out.println(i);
         }
     }
--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,8 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-        for (int i = 0; i <= power; i++) {
-            System.out.println(Math.pow(2, i));
+        if (power < 0)
+            System.out.println("too much power");
+        else {
+            for (int i = 0; i <= power; i++) {
+                System.out.println((int) Math.pow(2, i));
+            }
         }
     }
 }
