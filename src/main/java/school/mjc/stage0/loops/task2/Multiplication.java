823efc77ce24fa3b84ce503625dab3d619a29f6d
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-07-30 18:04:49 +0500

    hwllo

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,10 +2,8 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        if (multiplyByAndToInclusive > 0) {
             for (int i = 0; i <= Math.abs(multiplyByAndToInclusive); i++) {
                 System.out.println(i * multiplyByAndToInclusive);
             }
-        }
     }
 }
